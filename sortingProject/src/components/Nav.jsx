f9b295ad4f7f6afa4e2ff5844b1a5bb794f77a0e
08abhinav <abhinavnegi101@gmail.com>
2025-05-21 18:43:12 +0530

    update: style updation

--- sortingProject/src/components/Nav.jsx
+++ sortingProject/src/components/Nav.jsx
@@ -1,36 +1,39 @@
 import React from 'react';
+import FloatingParicles from './FloatingParicles';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
-    <nav className="bg-[#1e1e1e] text-white shadow-md fixed top-0 left-0 w-full z-50">
+    <nav className="bg-[#000] text-white shadow-md fixed top-0 left-0 w-full z-50">
       <div className="max-w-screen-xl mx-auto px-4 py-5 flex items-center justify-between">
         
         {/* Left: Logo */}
         <div className="flex items-start space-x-2">
           <div className="text-white font-bold text-2xl">🔢</div>
           <div className='flex flex-col'>
-            <span className="text-white text-2xl font-mono leading-tight">Sortify</span>
-            <span className='text-sm text-white font-mono'>Sorting Algorithms, Made Simple.</span>
+            <span className="text-white text-2xl font-mono leading-tight font-semibold">Sortify</span>
+            <span className='text-sm text-white font-mono font-semibold'>Sorting Algorithms, Made Simple.</span>
           </div>
           
         </div>
 
         {/*  Nav Links  */}
         <div className="hidden md:flex space-x-6 text-sm font-medium">
-          <a href="#" className="relative group text-white text-lg font-mono hover:text-gray-400 transition duration-400">
+          <Link to="/" className="relative group font-semibold text-white text-lg font-mono hover:text-gray-400 transition duration-400">
             Home
             <span className='absolute left-0 -bottom-1 h-0.5 w-0 bg-gray-400 transition-all duration-400 group-hover:w-full'></span>
-          </a>
-          <a href="#" className="relative group text-white text-lg font-mono hover:text-gray-400 transition duration-400">
+          </Link>
+          <Link to="/about-us" className="relative group font-semibold text-white text-lg font-mono hover:text-gray-400 transition duration-400">
             About
             <span className='absolute left-0 -bottom-1 h-0.5 w-0 bg-gray-400 transition-all duration-400 group-hover:w-full'></span>
-          </a>
-          <a href="#" className="relative group text-white text-lg font-mono hover:text-gray-400 transition duration-400">
+          </Link>
+          <Link to="/visualizer" className="relative group font-semibold text-white text-lg font-mono hover:text-gray-400 transition duration-400">
             Visualizer
             <span className='absolute left-0 -bottom-1 h-0.5 w-0 bg-gray-400 transition-all duration-400 group-hover:w-full'></span>
-          </a>
+          </Link>
         </div>
       </div>
+      <FloatingParicles/>
     </nav>
   );
 };
