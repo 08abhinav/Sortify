efa521d31f98d97e778ed1d3b8ea18cafb87e924
08abhinav <abhinavnegi101@gmail.com>
2025-05-23 18:34:37 +0530

    update: code updated for highlighting the psuedocode and bars

--- sortingProject/src/algorithms/bubbleSort.js
+++ sortingProject/src/algorithms/bubbleSort.js
@@ -1,26 +1,46 @@
-import { delay } from "@/util/delay"
-export async function bubbleSort(array, setArray, speed){
-    const arr = [...array]
-    const n = arr.length
+import { delay } from "@/util/delay";
 
-    for(let i=0;i<n-1;i++){
-        for(let j=0;j<n-i-1;j++){
-            arr[j].state = 'compare'
-            arr[j+1].state = 'compare'
-            setArray([...arr])
-            await delay(speed);
+export const bubbleSort = async (array, setArray, speed, setCurrentLine) => {
+  let arr = [...array];
+  let n = arr.length
 
-            if(arr[j].value > arr[j+1].value){
-                [arr[j], arr[j+1]] =  [arr[j+1], arr[j]]
-                arr[j].state = 'swap';
-                arr[j+1].state = 'swap';
-                setArray([...arr]);
-                await delay(speed)
-            }
-            arr[n-i-1].state = 'sorted';
-            setArray([...arr])
-        }
+  for (let i = 0; i < n; i++) {
+    setCurrentLine(0);
+    await delay(speed);
+
+    for (let j = 0; j < n - i - 1; j++) {
+      setCurrentLine(1);
+      
+      arr[j].state = 'compare';
+      arr[j+1].state = 'compare';
+      setArray([...arr])
+      await delay(speed);
+        
+      if (arr[j] && arr[j + 1] && arr[j].value > arr[j + 1].value) {
+        setCurrentLine(2);
+        await delay(speed);
+
+        const temp1 = { ...arr[j] };
+        const temp2 = { ...arr[j + 1] };
+
+        arr[j] = temp2;
+        arr[j + 1] = temp1;
+
+        arr[j].state = 'swap';
+        arr[j + 1].state = 'swap';
+
+        setArray([...arr]);
+
+        setCurrentLine(3);
+        await delay(speed);
+
+        arr[j].state = 'default';
+        arr[j + 1].state = 'default';
+      }
+      arr[n-i-1].state = 'sorted';
+      setArray([...arr])
     }
-    arr[0].state = 'sorted';
-    setArray([...arr])
-}
+  }
+
+  setCurrentLine(null);
+};
