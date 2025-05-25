23281cdc5019279284a72a9e68e1f0cbc2d38cf0
08abhinav <abhinavnegi101@gmail.com>
2025-05-25 18:32:15 +0530

    implemented array of pseudocodes

--- sortingProject/src/data/pseudoCode.js
+++ sortingProject/src/data/pseudoCode.js
@@ -13,6 +13,40 @@ const algopseudoCode = {
     "arr[j+1] = arr[j]",
     "j--",
     "arr[j+1] = key"
+  ],
+  selection:[
+    "for i from 0 to n-2",
+    "minIndex = i",
+    "for j from i+1 to n-1",
+    "if arr[j] < arr[minIndex]",
+    "minIndex = j",
+    "swap arr[minIndex] and arr[i]"
+  ],
+  merge: [
+  "function mergeSort(arr, left, right)",
+    "if left >= right",
+    "    return",
+    "  mid = (left + right) / 2",
+    "  mergeSort(arr, left, mid)",
+    "  mergeSort(arr, mid + 1, right)",
+    "  merge(arr, left, mid, right)",
+    "",
+    "function merge(arr, left, mid, right)",
+    "  while i <= mid and j <= right",
+    "    if arr[i] <= arr[j]",
+    "      temp.push(arr[i])",
+    "      i++",
+    "    else",
+    "      temp.push(arr[j])",
+    "      j++",
+    "  while i <= mid",
+    "    temp.push(arr[i])",
+    "    i++",
+    "  while j <= right",
+    "    temp.push(arr[j])",
+    "    j++",
+    "  for k = left to right",
+    "    arr[k] = temp[k - left]"
   ]
 }
 
