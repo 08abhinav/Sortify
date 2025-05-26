e82cbc95f46fb352fc8cc772c90624a1fb6b4ceb
08abhinav <abhinavnegi101@gmail.com>
2025-05-26 15:54:58 +0530

    update: added more infos about algo

--- sortingProject/src/components/AlgoInfo.jsx
+++ sortingProject/src/components/AlgoInfo.jsx
@@ -19,16 +19,27 @@ const AlgorithmInfo = ({onClose}) => {
     <div className="fixed top-0 left-0 w-full h-full bg-black/80 backdrop-blur-sm z-50 flex items-center justify-center">
       <div className="bg-zinc-900 rounded-2xl shadow-lg p-8 max-w-2xl text-white relative">
         <h2 className="text-3xl font-bold font-mono mb-4 underline">{algorithm.name}</h2>
-        <p className="mb-6 text-gray-300">{algorithm.description}</p>
-
-        {/* Optional: Legend */}
+        <p className="mb-4 text-gray-200 font-mono">{algorithm.description}</p>
         <div className="mb-6">
-          <h3 className="text-lg font-semibold mb-2">Color Legend:</h3>
+          <h3 className="text-lg font-semibold mb-2 underline">Time Complexity:</h3>
+          <table className="text-sm text-left w-full border-collapse">
+            <tbody>
+              <tr><td className=" text-gray-200 font-mono">Best Case:</td><td>{algorithm.complexities.best}</td></tr>
+              <tr><td className=" text-gray-200 font-mono">Average Case:</td><td>{algorithm.complexities.average}</td></tr>
+              <tr><td className=" text-gray-200 font-mono">Worst Case:</td><td>{algorithm.complexities.worst}</td></tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div className="mb-6">
+          <h3 className="text-lg font-semibold mb-2 font-mono underline">Bar States Guide:</h3>
           <ul className="space-y-1 text-sm">
-            <li><span className="inline-block w-4 h-4 bg-blue-400 mr-2"></span> Comparing</li>
-            <li><span className="inline-block w-4 h-4 bg-green-400 mr-2"></span> Sorted</li>
-            <li><span className="inline-block w-4 h-4 bg-yellow-400 mr-2"></span> Pivot (Quick Sort)</li>
-            <li><span className="inline-block w-4 h-4 bg-red-400 mr-2"></span> Swapping</li>
+            {algorithm.legend?.map((item, idx)=>(
+              <li key={idx} className='font-mono'>
+                <span className={`inline-block w-4 h-4 ${item.color} mr-2 rounded-sm`}></span>
+                {item.label}
+              </li>
+            ))}           
           </ul>
         </div>
 
