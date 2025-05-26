3b40471a72aaacec438d1d88f9008a72c5b66218
08abhinav <abhinavnegi101@gmail.com>
2025-05-26 21:17:53 +0530

    initial commit

--- sortingProject/vite.config.js
+++ sortingProject/vite.config.js
@@ -9,4 +9,12 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  server: {
+    port: 3000,
+    host: true,
+  },
+  preview: {
+    port: process.env.PORT || 3000,
+    host: true,
+  },
 });
